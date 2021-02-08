71064f800d32a32bbfa900ef9254ce46eb52ffd7
Lukas Reining <lukas.reining@codecentric.de>
2021-02-08 14:56:41 +0000

    Add timeout to GroveUART_Read

--- MT3620_Grove_Shield_Library/HAL/GroveUART.c
+++ MT3620_Grove_Shield_Library/HAL/GroveUART.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sys/select.h>
 
 #include <applibs/uart.h>
 
@@ -14,18 +15,38 @@ int GroveUART_Open(UART_Id id, UART_BaudRate_Type baudRate)
 	return UART_Open(id, &uartConfig);
 }
 
-void GroveUART_Write(int fd, const uint8_t* data, int dataSize)
-{	
+void GroveUART_Write(int fd, const uint8_t *data, int dataSize)
+{
 	write(fd, data, (size_t)dataSize);
 }
 
-bool GroveUART_Read(int fd, uint8_t* data, int dataSize)
+bool GroveUART_Read(int fd, uint8_t *data, int dataSize)
 {
 	int totalReadSize = 0;
+	fd_set set;
+
+	struct timeval timeout = {
+		.tv_sec = 0,
+		.tv_usec = 10000};
+
+	FD_ZERO(&set);
+	FD_SET((size_t) fd, &set);
+
 	do
 	{
+		int rv = select(fd + 1, &set, NULL, NULL, &timeout);
+
+		if (rv < 1)
+		{
+			return false;
+		}
+
 		int readSize = read(fd, &data[totalReadSize], (size_t)(dataSize - totalReadSize));
-		if (readSize <= 0) return false;
+		if (readSize < 0)
+		{
+			return false;
+		}
+
 		totalReadSize += readSize;
 	} while (totalReadSize < dataSize);
 
